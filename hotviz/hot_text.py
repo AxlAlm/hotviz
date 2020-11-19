81326dc4331d12bfdd1f2befeec02f1073e75cc5
Axel <axelalmquist@gmail.com>
2020-11-19 11:49:54 +0100

    hottext is almsot done. need to figure out a prettier way to label the gold and pred spans so the sup/sub doesnt overlap with text or lines

--- hotviz/hot_text.py
+++ hotviz/hot_text.py
@@ -5,112 +5,135 @@ import matplotlib.colors as colors
 import json
 import imgkit
 import base64
+from lxml import etree, html
+#from bs4 import BeautifulSoup as bs
+from IPython.display import Image
 
-    
-def hot_text(tokens, class_scores, preds, true, th_path):
-    
-    print(tokens)
+
+def class_header(labels:list, style_elems:list):
+    header = ["<span> Classes: </span>"] 
+    label2cmap = {}
     color_ranges = ["Reds", "Blues", "Greens", "Oranges", "Purples"]
+    for i,label in enumerate(labels):
+        cmap = cm.get_cmap(color_ranges[i])
+        hex_code = colors.to_hex(cmap(1.0))
 
-    style_elems = []
-    task2colors = {}
-    task_marks = []
+        style_elems.append(f'.c{label} {{ background-color: {hex_code}; }}')
+        header.append(f'<span class="c{label}">  {label}  </span><span> |</span>')
+        label2cmap[label] = cmap
+
+    header.append("<br><br>")
+    return header, label2cmap
+
+    
+def hot_text(data, labels:list, save_path:str="/tmp/hot_text.png", display:bool=True, return_html:bool=True):
+    
+    # this needs to be improved..
+    style_elems = [ 
+                    "span { line-height: 54px;}",
+
+                    # ".start_pred {border: 2px solid red; padding: 5px; border-top:1px solid transparent; border-right:1px solid transparent;}",
+                    # ".end_pred {border: 2px solid red; padding: 5px; border-top:1px solid transparent; border-left:1px solid transparent;}",
+                    # ".mid_pred {border: 2px solid red; padding: 5px; border-top:1px solid transparent; border-left:1px solid transparent; border-right:1px solid transparent;}",
+                    # ".start_gold {border: 2px solid gold; padding: 5px; border-top:1px solid transparent; border-right:1px solid transparent;}",
+                    # ".end_gold {border: 2px solid gold; padding: 5px; border-top:1px solid transparent; border-left:1px solid transparent;}",
+                    # "mid_gold {border: 2px solid gold; padding: 5px; border-top:1px solid transparent; border-left:1px solid transparent; border-right:1px solid transparent;}",
+
+                    "gold {color: gold;}",
+                    "red {color: red;}",
+                    #".verticalLineGold { border-left: thick solid gold;}"
+                    #".verticalLinePred { border-left: thick solid gold;}"
+                    #".gold {text-decoration:overline; text-decoration-color:gold;, padding-top: 10px;}",
+                    ".gold {border-top: 3px solid gold; padding-top: 10px;}",
+                    ".pred {border-bottom: 3px solid red; padding-bottom: 10px;}",
+                    ".gold_pred {border-bottom: 3px solid red; padding-bottom: 10px; border-top: 3px solid gold; padding-top: 10px;}",
+                    ".supsub {position: absolute}",
+                    ".subscript {display:block; position:relative; left:2px; top: 30px}",
+                    ".superscript {display:block; position:relative; left:2px; top:-30px}",
+                    ]
 
     # setting the color for each class and creating a header where you can see the color for each class
-    if class_scores:
-        task_marks.append("<span> Classes: </span>")
-        for i,(task, scores) in enumerate(class_scores.items()):
-            cmap = cm.get_cmap(color_ranges[i])
-            off = (sum(scores) / len(scores)) * 0.0001
-            normer = colors.Normalize(vmin=0-off, vmax=1.2+off)
-            task_colors = [colors.to_hex(cmap(normer(x))) for x in scores]
-            task2colors[task] = task_colors
+    #use_scores = "False"
+    #if "score" in data[0]["pred"]:
+    header, label2cmap = class_header(labels, style_elems)
 
-            style_elems.append(f'.c{task} {{ background-color: {colors.to_hex(cmap(0.8))}; }}')
-            task_marks.append(f'<span class="c{task}">{task}</span> <span> |</span>')
-
-        task_marks.append("<br><br>")
-        
-
-    label2ID = {}
-    label2currentlabel = {}
+    gold_spans = []
     pred_spans = []
     token_stack = []
-    current_span_label = None
-    for i in range(len(tokens)):
+
+    last_pred_span = ""
+    last_gold_span = ""
+    for i,td in enumerate(data):
+
+        pred = td["pred"]
+        gold = td.get("gold", {"label":None, "span_id":None})
+        token = td["token"]
+        if "score" in pred and "label" in pred:
+            if pred["score"] != None and pred["label"] != None:
+                cmap = label2cmap[pred["label"]]
+                hex_code = colors.to_hex(cmap(pred["score"]))
+                style_elems.append(f'.c{i} {{ background-color: {hex_code}; }}')
+                token = f'<span class="c{i}">{token} </span>'
+
+
+        if pred["label"] and gold["label"]:
+            token = f'<span class="gold_pred">{token}</span>'
+        elif pred["label"]:
+            token = f'<span class="pred">{token}</span>'
+
+        elif gold["label"]:
+            token = f'<span class="gold">{token}</span>'
+
+        # we got a new span
+        # if gold["span_id"] != last_gold_span and not last_gold_span:
+        #     token_stack.append("|")
+
+        # if we have a new span that is not the first for pred
+        if last_gold_span != gold["span_id"] and last_gold_span:
+            pass
+            #<gold>|</gold>
+            #<span class='supsub'><sup class='superscript'>Sup</sup></span>
+            #sup = f"<span class='supsub'><sup class='superscript'>{last_gold_span}</sup></span>"
+            #token_stack.append(sup)
+
+        # if we have a new span that is not the first for gold
+        if last_pred_span != pred["span_id"] and last_pred_span:
+            pass
+            #<gold>|</gold>
+            #sub = f"<span class='supsub'><sub class='subscript'>{last_pred_span}</sub></span>"
+            #token_stack.append(sub)
         
-        if class_scores:
-            top_class = ""
-            highest_prob = -1
-            for task, scores in class_scores.items():
+        token_stack.append(token)
+        last_pred_span = pred["span_id"]    
+        last_gold_span = gold["span_id"]
 
-                if scores[i] > highest_prob:
-                    top_class = task
-                    highest_prob = scores[i]
-        
-            style_elems.append(f'.c{i} {{ background-color: {task2colors[top_class][i]}; }}')
-            token_stack.append(f'<span class="c{i}">{tokens[i]}</span>')
-        
-        else:
-            token_stack.append(tokens[i])
-            
-            
-        current_pred = preds[i]
-        SEG, *label = current_pred.split("_")
-        
-
-        try:
-            NEXT_SEG, *_  = preds[i+1]
-        except IndexError as e:
-            NEXT_SEG = "O"
-        
-        if not label:
-            label = "seg"
-        else:
-            label = label[0]
-
-        if label not in label2ID:
-            label2ID[label] = 0
-        
-        if SEG == "B":
-            pred_spans.extend(token_stack[:-1])
-            token_stack = [token_stack[-1]]
-
-            label2ID[label] += 1
-
-        current_span_label = f"{label}_{label2ID[label]}"
-
-        next_outside_or_new = NEXT_SEG in ["O", "B"]
-        current_inside_or_begin = SEG in ["I","B"]
-        if current_inside_or_begin and next_outside_or_new:
-            
-            pred_spans.append(f"|<u>{' '.join(token_stack)}</u>|<sup>{current_span_label}</sup>")
-
-            token_stack = []
-
-
-    pred_spans.extend(token_stack)        
-
+    #span {{ font-family:"verdana", monospace; font-size: 20px; }} 
     html_string =  f""" <html>
                             <head>
                                 <style>
-                                span {{ font-family:"verdana", monospace; font-size: 20px; }} 
                                 {' '.join(style_elems)}
                                 </style>
                             </head>
-                            <body>
+                            <body style="font-family:Verdana; font-size:20px;">
                             <br><br>
                             <br><br>
-                            {' '.join(task_marks)}
-                            {' '.join(pred_spans)}
+                            {' '.join(header)}
+                            {' '.join(token_stack)}
+
+                            <br><br>
+
                             </body>
                             </html>
                     """
 
-    imgkit.from_string(html_string, th_path, options={'quiet':'', "width": 800, "height":600})
+    imgkit.from_string(html_string, save_path, options={'quiet':'', "width": 800, "height":600})
+    Image(save_path)
 
-    with open(th_path, "rb") as f:
-        enc_img = base64.b64encode(f.read())
-        src = f"data:image/png;base64,{enc_img.decode()}"
 
-    return src
+    document_root = html.fromstring(html_string)
+    print(etree.tostring(document_root, encoding='unicode', pretty_print=True))
+
+    # if return_html:
+    #     soup = bs(html_string, features="lxml")
+    #     pretty_html = soup.prettify()
+    #     return pretty_html
