21583fd50233272fc1206d19c58e0afdcf4cde61
Axel <axelalmquist@gmail.com>
2020-11-30 10:13:49 +0100

    changed readme, span example data

--- README.md
+++ README.md
@@ -20,3 +20,33 @@ hot_tree(your_pred_data, gold_data=your_gold_data)
 ```
 
 ![](https://github.com/AxlAlm/hotviz/blob/setup/hotviz/example/tree_example.gif)
+
+
+
+## HotText
+
+A text highlighter where colors reflect the scores given for each token. This can be useful when wanting to visualize the confidence scores for a given class or the attention in a nn.
+
+HotText also supports visulization of gold and predited spans so that one can see where the prediction differs from the gold.
+
+
+```python
+from hotviz import hot_text
+from hotviz.example import span_data
+
+hmtl = hot_text(    
+                span_data, 
+                labels=["X", "Z"], 
+                save_path="/tmp/hot_text.png",
+                print_html=False, 
+                show_spans=True,
+                show_scores=True
+                )
+```
+
+![](https://github.com/AxlAlm/hotviz/blob/setup/hotviz/example/hot_text_example.png)
+
+TODO: 
+- changing color of hot_text
+- changing possition of span label text of hot_text
+- adding breaks in spans for hot_text
--- hotviz/example/span_data.py
+++ hotviz/example/span_data.py
@@ -1,67 +1,3 @@
-tree_data = [
-                        {'label': 'MajorClaim',
-                        'link': 1,
-                        'link_label': '',
-                        'text': 'one who studies overseas will gain many skills throughout this '
-                                'experience'},
-                        {'label': 'MajorClaim',
-                        'link': 1,
-                        'link_label': '',
-                        'text': 'living and studying overseas gives the individual a new perspective '
-                                'on the subject that is studied or in general life'},
-                        {'label': 'Claim',
-                        'link': 1,
-                        'link_label': 'For',
-                        'text': 'studying at an overseas university gives individuals the '
-                                'opportunity to improve social skills by interacting and '
-                                'communicating with students from different origins and cultures'},
-                        {'label': 'Claim',
-                        'link': 1,
-                        'link_label': 'For',
-                        'text': 'living and studying overseas is an irreplaceable experience when it '
-                                'comes to learn standing on your own feet'},
-                        {'label': 'Claim',
-                        'link': 1,
-                        'link_label': 'For',
-                        'text': 'one who has studied and lived overseas will become more eligible '
-                                'for the job than his/her peers'},
-                        {'label': 'Premise',
-                        'link': 2,
-                        'link_label': 'Supports',
-                        'text': 'Compared to the peers studying in the home country, it will be more '
-                                'likely for the one who is living overseas to be successful in '
-                                'adapting himself/herself into new environments and situations in '
-                                'life'},
-                        {'label': 'Premise',
-                        'link': 3,
-                        'link_label': 'Attacks',
-                        'text': 'One who is living overseas will of course struggle with loneliness, '
-                                'living away from family and friends'},
-                        {'label': 'Premise',
-                        'link': 6,
-                        'link_label': 'Attacks',
-                        'text': 'those difficulties will turn into valuable experiences in the '
-                                'following steps of life'},
-                        {'label': 'Premise',
-                        'link': 3,
-                        'link_label': 'Supports',
-                        'text': 'the one will learn living without depending on anyone else'},
-                        {'label': 'Premise',
-                        'link': 4,
-                        'link_label': 'Supports',
-                        'text': 'employers are mostly looking for people who have international and '
-                                'language skills'},
-                        {'label': 'Premise',
-                        'link': 4,
-                        'link_label': 'Supports',
-                        'text': 'Becoming successful in this study will give the student an edge in '
-                                'job market'},
-                        {'label': 'Claim',
-                        'link': 1,
-                        'link_label': 'Against',
-                        'text': 'there are many difficulties a student might face when studying and '
-                                'living overseas'}
-                        ]
 
 
 span_data = [
@@ -69,15 +5,16 @@ span_data = [
                                 "token": "I",
                                 "pred": {
                                             "span_id": None,
-                                            "label": None,
+                                            "label": "X",
+                                            "score": 0.1,                                            
                                             },
                         },
                         {
                                 "token": "think",
                                 "pred": {
                                             "span_id": None,
-                                            "label": None,
-                                            "score":None,
+                                            "label": "X",
+                                            "score": 0.1,
 
                                             },
                         },
@@ -85,8 +22,8 @@ span_data = [
                                 "token": "that",
                                 "pred": {
                                             "span_id": None,
-                                            "label": None,
-                                            "score": None,
+                                            "label": "X",
+                                            "score": 0.1,
 
                                             },
                                 "gold": {
@@ -159,8 +96,8 @@ span_data = [
                                 "token": "and",
                                 "pred": {
                                             "span_id": None,
-                                            "label": None,
-                                            "score": None,
+                                            "label": "X",
+                                            "score": 0.2,
                                             },
                         },
                             {
@@ -245,15 +182,16 @@ span_data = [
                                 "token": ".",
                                 "pred": {
                                             "span_id": None,
-                                            "label": None,
-                                            "score": None,
+                                            "label": "Z",
+                                            "score": 0.1,
                                             },
                         },
                         {
                                 "token": "Lastly,",
                                 "pred": {
                                             "span_id": None,
-                                            "label": None,
+                                            "label": "Z",
+                                            "score": 0.1,
                                             },
                         },
                             {
@@ -280,19 +218,19 @@ span_data = [
                                             "score": 0.8,
                                             },
                                 "gold": {
-                                            "span_id": "Z222222_2",
+                                            "span_id": "Z_2",
                                             "label": "Z",
                                         }
                         },
                         {
                                 "token": "number",
                                 "pred": {
-                                            "span_id": "Z_3",
+                                            "span_id": "Z_2",
                                             "label": "Z",
                                             "score": 0.9,
                                             },
                                 "gold": {
-                                            "span_id": "Z222222_2",
+                                            "span_id": "Z_2",
                                             "label": "Z",
                                         }
                         },
@@ -304,7 +242,7 @@ span_data = [
                                             "score": 0.8,
                                             },
                                 "gold": {
-                                            "span_id": "Z222222_2",
+                                            "span_id": "Z_2",
                                             "label": "Z",
                                         }
                         },
