209d00e8997c7781cd08695356783fa1e381233e
Axel <axelalmquist@gmail.com>
2021-01-04 12:00:00 +0100

    changed back to previous solution of handling package metadata

--- setup.py
+++ setup.py
@@ -1,26 +1,23 @@
 from setuptools import find_packages, setup
-import json
-import pathlib
-import os
 
+try:
+    import builtins
+except ImportError:
+    import __builtin__ as builtins
 
-def get_meta():
-    meta_file = os.path.join(pathlib.Path(__file__).parent.absolute(), "meta.json")
-    with open(meta_file, "r") as f:
-        meta = json.load(f)
-    return meta
+builtins.__HOTVIZ_SETUP__ = True
 
-meta = get_meta()
+import hotviz
 
 
 setup(
     name='hotviz',
-    version=meta["__version__"],    
+    version=hotviz.__version__,    
     description='A visulizations lib for python',
     url='https://github.com/AxlAlm/hotviz',
-    author=meta["__author__"],
-    author_email=meta["__author_email__"],
-    license=meta["__license__"],
+    author=hotviz.__author__,
+    author_email=hotviz.__author_email__,
+    license=hotviz.__license__,
     packages=find_packages(),
     install_requires=[
                         'numpy==1.18.1',
